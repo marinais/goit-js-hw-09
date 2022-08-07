a4bd0e8c515daa719c66a7dfeeae0840f330f5b5
Marina Ishchenko <marina.s.ishchenko@gmail.com>
2022-08-06 20:47:29 -0700

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startButtonEl = document.querySelector("[data-start]")
+const stopButtonEl = document.querySelector("[data-stop]")
 
+let timerId = null
+
+startButtonEl.addEventListener("click", () => {
+    timerId = setInterval(bodyColor, 1000),
+    document.querySelector("[data-start]").disabled = true
+})
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function bodyColor() {
+    document.body.style.backgroundColor = getRandomHexColor()
+    
+}
+
+stopButtonEl.addEventListener("click", stopEventListener)
+
+
+function stopEventListener() {
+    clearInterval(timerId)
+    startButtonEl.removeEventListener("click", bodyColor),
+    document.querySelector("[data-start]").disabled = false
+}
